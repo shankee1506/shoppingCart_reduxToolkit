a7657c774e001336a91dd9da73e54c4682bd6b18
shankee1506 <pshankeerthan@gmail.com>
2023-06-12 17:05:39 +0530

    add jenkins

--- src/products.js
+++ src/products.js
@@ -15,7 +15,7 @@ const products = [
       desc: "5.4-inch mini display",
       price: 699,
       image:
-        "https://telstra.com.au/content/dam/tcom/devices/mobile/mhdwhst-i12m/black/front.png",
+        "https://d1eh9yux7w8iql.cloudfront.net/product_images/413834_ea6b8fe1-b521-48dd-8169-522add6ea34b.jpg",
     },
     {
       id: 3,
@@ -25,7 +25,88 @@ const products = [
       price: 399,
       image:
         "https://www.androidauthority.com/wp-content/uploads/2020/01/Samsung-Galaxy-S2.jpg",
-    },
+  },
+  {
+    id: 1,
+    name: "iPhone 12 Pro",
+    brand: "Apple",
+    desc: "6.1-inch display",
+    price: 999,
+    image:
+      "https://d1eh9yux7w8iql.cloudfront.net/product_images/413834_ea6b8fe1-b521-48dd-8169-522add6ea34b.jpg",
+  },
+  {
+    id: 2,
+    name: "iPhone 12",
+    brand: "Apple",
+    desc: "5.4-inch mini display",
+    price: 699,
+    image:
+      "https://d1eh9yux7w8iql.cloudfront.net/product_images/413834_ea6b8fe1-b521-48dd-8169-522add6ea34b.jpg",
+  },
+  {
+    id: 3,
+    name: "Galaxy S",
+    brand: "Samsung",
+    desc: "6.5-inch display",
+    price: 399,
+    image:
+      "https://www.androidauthority.com/wp-content/uploads/2020/01/Samsung-Galaxy-S2.jpg",
+  },
+  {
+    id: 1,
+    name: "iPhone 12 Pro",
+    brand: "Apple",
+    desc: "6.1-inch display",
+    price: 999,
+    image:
+      "https://d1eh9yux7w8iql.cloudfront.net/product_images/413834_ea6b8fe1-b521-48dd-8169-522add6ea34b.jpg",
+  },
+  {
+    id: 2,
+    name: "iPhone 12",
+    brand: "Apple",
+    desc: "5.4-inch mini display",
+    price: 699,
+    image:
+      "https://d1eh9yux7w8iql.cloudfront.net/product_images/413834_ea6b8fe1-b521-48dd-8169-522add6ea34b.jpg",
+  },
+  {
+    id: 3,
+    name: "Galaxy S",
+    brand: "Samsung",
+    desc: "6.5-inch display",
+    price: 399,
+    image:
+      "https://www.androidauthority.com/wp-content/uploads/2020/01/Samsung-Galaxy-S2.jpg",
+},
+{
+  id: 1,
+  name: "iPhone 12 Pro",
+  brand: "Apple",
+  desc: "6.1-inch display",
+  price: 999,
+  image:
+    "https://d1eh9yux7w8iql.cloudfront.net/product_images/413834_ea6b8fe1-b521-48dd-8169-522add6ea34b.jpg",
+},
+{
+  id: 2,
+  name: "iPhone 12",
+  brand: "Apple",
+  desc: "5.4-inch mini display",
+  price: 699,
+  image:
+    "https://d1eh9yux7w8iql.cloudfront.net/product_images/413834_ea6b8fe1-b521-48dd-8169-522add6ea34b.jpg",
+},
+{
+  id: 3,
+  name: "Galaxy S",
+  brand: "Samsung",
+  desc: "6.5-inch display",
+  price: 399,
+  image:
+    "https://www.androidauthority.com/wp-content/uploads/2020/01/Samsung-Galaxy-S2.jpg",
+},
   ];
   
   module.exports = products;
